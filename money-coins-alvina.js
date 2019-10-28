a3a44baeae9d29134ae6b03c0523180de09235dc
memorvin <alvina.tjia@yahoo.com>
2019-10-28 15:28:22 +0700

    update money-coins

--- money-coins-alvina.js
+++ money-coins-alvina.js
@@ -1,9 +1,6 @@
 // function convertToCoin(n) {
 //   let ref = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
 //   let result = [];
-//   if (n <= 0) {
-//     return result;
-//   }
 //   for (let i = 0; i < ref.length; i++) {
 //     while (n >= ref[i]) {
 //       result.push(ref[i]);
@@ -20,14 +17,16 @@ function convertToCoin(n) {
   let result = [];
   if (n <= 0) {
     return result;
-  }
-  for (let i = 0; i < ref.length; i++) {
-    if (n >= ref[i]) {
-      result.push(ref[i]);
-      return result.concat(convertToCoin(n-ref[i]));
+  } else {
+    for (let i = 0; i < ref.length; i++) {
+      if (n >= ref[i]) {
+        result.push(ref[i]);
+        return result.concat(convertToCoin(n-ref[i]));
+      }
     }
   }
 }
 */
 
 console.log(convertToCoin(37454));
+console.log(convertToCoin(7752));
