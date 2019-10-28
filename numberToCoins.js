04947807802ac9523ef0b92daae843c9cce24534
Abdullah Said <34899210+abdullzz@users.noreply.github.com>
2019-10-29 00:51:30 +0700

    Abdullah Said - numberToCoins.js

---
+++ numberToCoins.js
@@ -0,0 +1,15 @@
+function numberToCoin(num){
+    result = []
+    pecahan = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
+    
+    for(i = 0; i < pecahan.length; i++) 
+    {
+        while(num >= pecahan[i]) {
+            result.push(pecahan[i])
+            num -= pecahan[i]
+        }
+    }
+    return result
+};
+
+console.log(numberToCoin(312))
